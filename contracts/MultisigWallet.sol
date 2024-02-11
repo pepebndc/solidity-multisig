db4ef76ba18d082c487912dcd4eacbfa07668474
Youssef Srondy <95218070+srondss@users.noreply.github.com>
2024-02-11 07:13:23 +0100

    ERC1155 Support
    Enable Multisig Wallet to receive ERC1155 NFTs

--- contracts/MultisigWallet.sol
+++ contracts/MultisigWallet.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract MultisigWallet {
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract MultisigWallet is ERC1155Holder {
     event Deposit(address indexed sender, uint amount, uint balance);
     event SubmitTransaction(
         address indexed owner,
