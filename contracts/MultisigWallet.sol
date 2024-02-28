532416449f5cf6ba2d88e2a2ede13e6889da81f3
Pepe Blasco <pepeblascondc@gmail.com>
2024-02-28 16:48:03 +0100

    Added functions to change owners

--- contracts/MultisigWallet.sol
+++ contracts/MultisigWallet.sol
@@ -79,6 +79,35 @@ contract MultisigWallet is ERC1155Holder, ERC721Holder {
         numConfirmationsRequired = _numConfirmationsRequired;
     }
 
+    function changeConfig(address[] memory _owners, uint _numConfirmationsRequired) public {
+        require(msg.sender == address(this), "not sent over multisig");
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
+            "invalid number of required confirmations"
+        );
+
+        // remove current owners
+        for (uint i = 0; i < owners.length; i++) {
+            isOwner[owners[i]] = false;
+        }
+
+        delete owners;
+
+        // add new owners
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
     receive() external payable {
         emit Deposit(msg.sender, msg.value, address(this).balance);
     }
